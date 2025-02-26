2ec94fd7d67b8acf3c659cc6dbba6223f59ec237
marwaZyada <zyadamarwa6@gmail.com>
2025-02-26 23:36:25 +0200

    Add project files.

---
+++ Domain/Entity/LogCategory.cs
@@ -0,0 +1,25 @@
+﻿
+using Domain.Entity.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entity
+{
+	public class LogCategory
+	{
+        public Guid Id { get; set; }
+        public string Action { get; set; }
+        public DateTime Date { get; set; }
+        public string ApplicationUserId { get; set; }
+        //[ForeignKey("ApplicationUserId")]
+        //public ApplicationUser ApplicationUser { get; set; }
+        public Guid CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+      
+    }
+}
---
+++ Infrastructure/Data/FreeBookDbContext.cs
@@ -0,0 +1,46 @@
+﻿using Domain.Entity;
+using Domain.Entity.ViewModel;
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+	public class FreeBookDbContext:IdentityDbContext<ApplicationUser>
+	{
+        public FreeBookDbContext(DbContextOptions<FreeBookDbContext> options):base(options) 
+        {
+            
+        }
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+
+			builder.Entity<Category>().Property(c => c.Id).HasDefaultValueSql("(NEWID())");
+			builder.Entity<LogCategory>().Property(c => c.Id).HasDefaultValueSql("(NEWID())");
+			builder.Entity<SubCategory>().Property(c => c.Id).HasDefaultValueSql("(NEWID())");
+			builder.Entity<LogSubCategory>().Property(c => c.Id).HasDefaultValueSql("(NEWID())");
+			builder.Entity<Book>().Property(c => c.Id).HasDefaultValueSql("(NEWID())");
+			builder.Entity<LogBook>().Property(c => c.Id).HasDefaultValueSql("(NEWID())");
+			builder.Entity<IdentityUserLogin<string>>().HasNoKey();
+			builder.Entity<IdentityUserRole<string>>().HasNoKey();
+			builder.Entity<IdentityUserToken<string>>().HasNoKey();
+			//builder.Entity<Book>().Property(b=>b.CategoryId).IsRequired(false);
+			//builder.Entity<Book>().Property(b=>b.SubCategoryId).IsRequired(false);
+	
+
+		}
+		public DbSet<Category> Categories { get; set; }
+		public DbSet<LogCategory> LogCategories { get; set; }
+		public DbSet<SubCategory> SubCategories { get; set; }
+		public DbSet<LogSubCategory> LogSubCategories { get; set; }
+		public DbSet<Book> Books { get; set; }
+		public DbSet<LogBook> LogBooks { get; set; }
+	}
+}
